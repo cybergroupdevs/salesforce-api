a6834a085ddd868b29b07b3402a63aa59ccb847b
Kumar Navneet <kumarnavneet2006@gmail.com>
2019-05-28 17:50:31 +0530

    Adding more fields to the case model

--- app/cases/case.js
+++ app/cases/case.js
@@ -2,16 +2,29 @@ const Sequelize = require("sequelize");
 const { sequelize } = require("../../db/db");
 
 const Case = sequelize.define('case', {
-    subject: {
+  casenumber: {
+    type: Sequelize.STRING
+  },
+  closeddate: {
+    type: Sequelize.DATE
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  subject: {
       type: Sequelize.STRING
-    },
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true
-    },
-    createddate: {
-      type: Sequelize.DATE
-    }
+  },
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  status: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  createddate: {
+    type: Sequelize.DATE
+  }
 },
 {
   tableName: 'case',
